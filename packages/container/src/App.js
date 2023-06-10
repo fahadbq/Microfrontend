4af9d350fb1137d70e76bc875ec8f2624f62e5e8
Fahad <daylight1498@gmail.com>
2023-06-10 12:31:12 +0530

    Created container deploy script

---
+++ packages/container/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hii</h1>
+      <hr />
+      <MarketingApp />
+    </>
+  );
+};
+
+export default App;
