b96500d1890abc1c99f3ba81d13dd90fe0ceeebc
Fahad <daylight1498@gmail.com>
2023-06-15 00:22:32 +0530

    Added callback function to communicate with parent and child app

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,22 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./app";
 
-//Mount function to start up the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+// Mount function to start up the app
+const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
-// If we are in development and in isolation
-// call mount immediately.
+// If we are in development and in isolation,
+// call mount immediately
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
 // We are running through container
-// We should export the mount function
+// and we should export the mount function
 export { mount };
